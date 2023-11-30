8161a8431c51b9e7412bcc712fd8a7e5e24ad41a
abcaum006 <113670811+abcaum006@users.noreply.github.com>
2023-11-30 20:26:42 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        Foot foot;
-        int choice;
-
-        System.out.println("What to draw? 1. Ellipse, 2. Rectangle");
-        Scanner scanner = new Scanner(System.in);
-        choice = scanner.nextInt();
-
-        switch (choice) {
-            case 1:
-                foot = new Ellipse();
-                break;
-            case 2:
-                foot = new Rectangle();
-                break;
-            default:
-                System.out.println("Invalid choice");
-                return;
-        }
-
-        foot.draw();
+        Foot f = new Foot();
+        f.draw();
     }
 }
