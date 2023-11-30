42370f852a9dd0938bbf2a27c4a3535704245c97
abcaum006 <113670811+abcaum006@users.noreply.github.com>
2023-11-30 20:28:04 +0700

    Add files via upload

---
+++ FootShape.java
@@ -0,0 +1,11 @@
+public class FootShape {
+    public String drawAsEllipse() {
+        Ellipse e = new Ellipse();
+        return e.draw();
+    }
+
+    public String drawAsRectangle() {
+        Rectangle r = new Rectangle();
+        return r.draw();
+    }
+}
