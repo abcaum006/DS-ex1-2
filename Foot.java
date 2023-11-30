495a0cf31c654e44ef52093c232a8f0be3de2676
abcaum006 <113670811+abcaum006@users.noreply.github.com>
2023-11-30 20:26:12 +0700

    Foot.java

--- Foot.java
+++ Foot.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Foot {
+    FootShape fs = new FootShape();
 
     public void draw() {
-        System.out.println("draw Foot");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What to draw? 1. Ellipse, 2. Rectangle ");
+        int input = sc.nextInt();
+        if (input == 1) System.out.println(fs.drawAsEllipse());
+        else if (input == 2) System.out.println(fs.drawAsRectangle());
+        sc.close();
     }
 }
